ca375f2a8fc72bb4c7e12352308e3fe41ee815ec
Boldizsár Poór <49209650+boldar99@users.noreply.github.com>
2024-12-16 15:11:16 +0000

    Fix perceval (#82)
    * WIP: fixing perceval
    
    * Fixed
    
    * Fixed
    
    * New perceval API
    
    * Fix flake8

--- test/test_perceval.py
+++ test/test_perceval.py
@@ -1,22 +1,28 @@
 import pytest
 
 from optyx.zw import *
-from optyx.circuit import BS, Phase
+from optyx.circuit import BS, BBS, Phase
+import numpy as np
 
 unitary_circuits = [
 	BS >> Phase(1 / 4) @ Id(Mode(1)) >> BS.dagger(),
+	BS @ Id(1) >> Id(1) @ BS,
 ]
 non_unitary_circuits = [
 	BS >> Endo(2) @ Id(Mode(1)) >> BS.dagger(),
 	Create(1) @ Id(Mode(1)),
-	Create(1) @ Id(Mode(1)) >> BS,
+	Id(Mode(3)) >> Id(1) @ BS,
+	Id(Mode(2)) @ Create(1) @ Id(Mode(2)),
+	Id(Mode(1)) @ Create(1) @ Id(Mode(2)) >> Id(2) @ BBS(0.3),
 	Create(2) @ Id(Mode(1)) >> BS >> Select(2) @ Id(1),
 	Id(1) @ Create(1, 1) >> BS @ Id(Mode(1)) >> Id(Mode(2)) @ Select(1),
+	Create(1) @ Id(Mode(2)) >> BS @ Id(Mode(1)) >> Id(Mode(1)) @ BS ,
+	Id(1) @ Create(2, 2, 1) >> BBS(0.3) @ BBS(0.3) >> Id(1) @ BBS(0.3) @ Id(1)
 ]
 
 
 @pytest.mark.parametrize("circuit", unitary_circuits + non_unitary_circuits)
-@pytest.mark.parametrize("n_photons", range(1, 4))
+@pytest.mark.parametrize("n_photons", range(1,2))
 def test_perceval_probs_equivalence(circuit: Diagram, n_photons: int):
 	qpath_probs = circuit.to_path().prob(n_photons).normalise()
 	perceval_probs = circuit.to_path().prob(n_photons, with_perceval=True)
