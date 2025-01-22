b7b3397b0647dc23414056c292d2a4f13c1f0b68
giodefelice <giodefelice@protonmail.com>
2025-01-22 12:38:12 +0000

    add Measure, Encode, Discard and fix a bug in doubling method

--- optyx/channel.py
+++ optyx/channel.py
@@ -8,37 +8,33 @@ Examples
 A Channel is initialised by its Kraus map from `dom` to `cod @ env`. 
 
 >>> from optyx.lo import BS, Phase
->>> circ = BS @ Phase(0.25) >> Phase(0.25) @ BS
->>> channel = Channel(name='circuit', kraus=circ, dom=qmode ** 3, cod=qmode ** 3, env=optyx.Ty())
+>>> circ = Phase(0.25) @ BS @ Phase(0.56) >> BS @ BS
+>>> channel = Channel(name='circuit', kraus=circ, dom=qmode ** 4, cod=qmode ** 4, env=optyx.Ty())
 
 We can calculate the probability of an input-output pair:
 
->>> from optyx.zw import Create, Select
->>> state = Channel('state', Create(1, 1, 1))
->>> effect = Channel('effect', Select(2, 0, 1))
->>> prob = (state >> channel >> effect).double().to_zw().to_tensor().eval().array
->>> amp = (Create(1, 1, 1) >> circ >> Select(2, 0, 1)).to_zw().to_tensor().eval().array
 >>> import numpy as np
+>>> state = Channel('state', zw.Create(1, 0, 1, 0))
+>>> effect = Channel('effect', zw.Select(1, 0, 1, 0))
+>>> prob = (state >> channel >> effect).double().to_zw().to_tensor().eval().array
+>>> amp = (zw.Create(1, 0, 1, 0) >> circ >> zw.Select(1, 0, 1, 0)).to_zw().to_tensor().eval().array
 >>> assert np.allclose(prob, np.absolute(amp) ** 2)
 
-Measuring, discarding and encoding classical information are modeled by spiders.
+We can check that probabilities sum to 1:
 
->>> discard = Channel("Discard", kraus=optyx.Id(optyx.mode), dom=qmode, cod=Ty(), env=optyx.mode)
->>> assert discard.double() == optyx.Spider(2, 0, optyx.mode)
->>> encode = Channel("Encode", kraus = optyx.Id(optyx.mode), dom=mode, cod=qmode)
->>> measure = Channel("Measure", kraus = optyx.Id(optyx.mode), dom=qmode, cod=mode)
->>> decoherence = optyx.Spider(2, 1, optyx.mode) >> optyx.Spider(1, 2, optyx.mode)
->>> assert (measure >> encode).double() == decoherence
+>>> bell_state = Channel('Bell', optyx.Scalar(1/np.sqrt(2)) @ zx.Z(0, 2))
+>>> dual_rail = Channel('2R', optyx.dual_rail(2))
+>>> setup = bell_state >> dual_rail >> channel >> Discard(qmode ** 3) @ Measure(qmode)
+>>> assert np.isclose(sum(setup.double().to_zw().to_tensor().eval().array), 1)
 
-We can model a lossy optical channel and compute its probabilities:
+We can construct a lossy optical channel loss and compute its probabilities:
 
->>> import numpy as np
 >>> kraus = lambda nu: zw.W(2) >> zw.Endo(np.sqrt(nu)) @ zw.Endo(np.sqrt(1 - nu)) 
 >>> loss = lambda nu: Channel('Loss(' + str(nu) + ')', kraus(nu), dom=qmode, cod=qmode, env=optyx.mode)
 >>> eff = 0.95
->>> lossy_channel = channel >> loss(eff) @ loss(eff) @ loss(eff)
+>>> lossy_channel = channel >> loss(eff).tensor(*[loss(eff) for _ in range(3)])
 >>> lossy_prob = (state >> lossy_channel >> effect).double().to_zw().to_tensor().eval().array
->>> assert np.allclose(lossy_prob, prob * (eff ** 3))
+>>> assert np.allclose(lossy_prob, prob * (eff ** 2))
 """
 
 from __future__ import annotations
@@ -57,7 +53,7 @@ class Ob(symmetric.Ob):
         return False if self.name in ["qubit", "qmode"] else True
 
     @property
-    def single(self): 
+    def single(self):
         return optyx.Ty(self._classical[self.name])
 
     @property
@@ -112,8 +108,19 @@ class Circuit(symmetric.Diagram):
 
 
 class Channel(symmetric.Box, Circuit):
-    """A Channel is defined by its Kraus map, from dom.single() to cod.single() @ env and interpreted as a completely positive map by doubling.
-    Every classical object in dom is encoded and decoded using optyx.Spider, see `Channel.double'."""
+    """A Channel is defined by its Kraus map, from dom.single() to cod.single() @ env 
+    and interpreted as a completely positive map by doubling.
+    
+    Measuring, discarding and encoding classical information are modeled by spiders.
+
+    >>> discard = Channel("Discard", kraus=optyx.Id(optyx.mode), dom=qmode, cod=Ty(), env=optyx.mode)
+    >>> assert discard.double() == optyx.Spider(2, 0, optyx.mode)
+    >>> encode = Channel("Encode", kraus = optyx.Id(optyx.mode), dom=mode, cod=qmode)
+    >>> measure = Channel("Measure", kraus = optyx.Id(optyx.mode), dom=qmode, cod=mode)
+    >>> decoherence = optyx.Spider(2, 1, optyx.mode) >> optyx.Spider(1, 2, optyx.mode)
+    >>> assert (measure >> encode).double() == decoherence
+    
+    """
     def __init__(self, name, kraus, dom=None, cod=None, env=optyx.Ty()):
         assert isinstance(kraus, optyx.Diagram)
         if dom is None:
@@ -152,7 +159,27 @@ class Channel(symmetric.Box, Circuit):
         top_perm = optyx.Diagram.permutation(get_perm(len(top_spiders.cod)), top_spiders.cod)
         swap_env = optyx.Id(cod @ self.env) @ optyx.Diagram.swap(cod, self.env)
         discards = optyx.Id(cod) @ optyx.Diagram.spiders(2, 0, self.env) @ optyx.Id(cod)
-        bot_perm = optyx.Diagram.permutation(get_perm(2 * len(cod)), cod @ cod).dagger()
+        new_cod = optyx.Ty()
+        for i in range(len(cod)):
+            new_cod = new_cod @ cod[i] @ cod[i]
+        bot_perm = optyx.Diagram.permutation(get_perm(2 * len(cod)), new_cod).dagger()
         bot_spiders = get_spiders(self.cod).dagger()
         return top_spiders >> top_perm >> self.kraus @ self.kraus.conjugate() >> swap_env >> discards >> bot_perm >> bot_spiders
 
+class Measure(Channel):
+    """ Measuring a qubit or qmode corresponds to applying a 2 -> 1 spider in the doubled picture."""
+    def __init__(self, dom):
+        cod = Ty(*[Ob._classical[ob.name] for ob in dom.inside])
+        super().__init__(name='Measure', kraus=optyx.Id(dom.single()), dom=dom, cod=cod)
+
+
+class Encode(Channel):
+    """Encoding a bit or mode corresponds to applying a 1 -> 2 spider in the doubled picture."""
+    def __init__(self, dom):
+        cod = Ty(*[Ob._quantum[ob.name] for ob in dom.inside])
+        super().__init__(name='Encode', kraus=optyx.Id(dom.single()), dom=dom, cod=cod)
+
+class Discard(Channel):
+    """Discarding a qubit or qmode corresponds to applying a 2 -> 0 spider in the doubled picture."""
+    def __init__(self, dom):
+        super().__init__(name='Discard', kraus=optyx.Id(dom.single()), dom=dom, cod=Ty(), env=dom.single())
