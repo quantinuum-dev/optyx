130779bfe447aac984e51ddcc5dbc1e62a7f498c
Boldizsár Poór <49209650+boldar99@users.noreply.github.com>
2025-01-24 12:01:36 +0000

    Fix to_tensor and IndexableAmplitudes.conjugate (#90)
    Closes #87 and #89
    
    (cherry picked from commit 3d4c784f2489fa4270e2e3511046b54bd2854485)

--- optyx/optyx.py
+++ optyx/optyx.py
@@ -855,7 +855,7 @@ class Scalar(Box):
         self, input_dims: list[int] = None
     ) -> list[int]:
         """Determine the output dimensions"""
-        return [1]
+        return []
 
 
 class DualRail(Box):
