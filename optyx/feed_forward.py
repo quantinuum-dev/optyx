3d6002b3dc0cc1c86fbe6df5517f017197ae07a8
mateuszkupper <mateusz.kupper@gmail.com>
2025-03-25 11:42:03 +0000

    feed_forward module

---
+++ optyx/feed_forward.py
@@ -0,0 +1,12 @@
+# detectors
+    # photon threshold detectors
+    # photon number resolving detectors
+# classically controlled gates
+#  three classes taking any (suitable) gates as inputs and returning a new controlled gate:
+    # classically controlled gates are gates that are controlled by classical bits
+    # classically controlled gates with continuous parameters
+    # classically controlled gates with discrete parameters
+# classical functions on the outputs of detectors
+#  input are the functions between natural numbers, bits and real numbers
+    # to be used as an input to classically controlled gates
+
