355d5449766bc91eb0f8c8615a31d8771c65a7ac
mateuszkupper <mateusz.kupper@gmail.com>
2025-05-23 16:18:22 +0100

    refactored fusion measurements

--- optyx/circuit/hybrid/dual_rail/measurement.py
+++ optyx/circuit/hybrid/dual_rail/measurement.py
@@ -1,89 +1,110 @@
 from optyx.circuit.qmode.lo import Phase, HadamardBS
 from optyx.circuit.qmode.measurement import NumberResolvingMeasurement
-from optyx.circuit.classical.classical_circuit import DiscardMode
+from optyx.circuit.classical.classical_arithmetic import DiscardMode
 from optyx.diagram.channel import qmode, mode
 from optyx.diagram.optyx import Swap, Mode, Bit
 from optyx.diagram.lo import BS_hadamard
-from optyx.diagram.channel import Channel, qmode
+from optyx.diagram.channel import Channel, qmode, Circuit
 from optyx.circuit.classical.classical_functions import ClassicalFunction
 
-ZMeasurement = lambda alpha: (
-    qmode @ Phase(alpha) >>
-    HadamardBS >>
-    NumberResolvingMeasurement(2) >>
-    DiscardMode(1) @ mode
-)
 
-XMeasurement = lambda alpha: (
-    HadamardBS >>
-    ZMeasurement(alpha)
-)
+class ZMeasurement(Circuit):
+    def __new__(cls, alpha):
+        """
+        ZMeasurement circuit that performs a measurement in the Z basis
+        after applying a phase shift of alpha.
+        """
+        return (
+            qmode @ Phase(alpha) >>
+            HadamardBS >>
+            NumberResolvingMeasurement(2) >>
+            DiscardMode(1) @ mode
+        )
 
 
-kraus_map_fusion_I = (
-    Mode(1) @ Swap(Mode(1), Mode(1)) @ Mode(1) >>
-    Mode(1) @ BS_hadamard @ Mode(1) >>
-    Mode(2) @ Swap(Mode(1), Mode(1)) >>
-    Mode(1) @ Swap(Mode(1), Mode(1)) @ Mode(1)
-)
+class XMeasurement(Circuit):
+    def __new__(cls, alpha):
+        """
+        XMeasurement circuit that performs a measurement in the X basis
+        after applying a Hadamard beam splitter.
+        """
+        return (
+            HadamardBS >>
+            ZMeasurement(alpha)
+        )
 
-fusion_I = Channel(
-    "Fusion", kraus_map_fusion_I
-)
 
-def fusion_I_function(x):
-    """
-    A classical function that returns two bits based on an input x,
-    based on the classical logical for the Fusion type II circuit.
-    """
-    a = x[0]
-    b = x[1]
-    s = (a % 2) ^ (b % 2)
-    k = int(s*b + (1-s)*(1 - (a + b)/2))%2
-    return [s, k]
+class FusionTypeI(Circuit):
+    def __new__(cls):
+        kraus_map_fusion_I = (
+            Mode(1) @ Swap(Mode(1), Mode(1)) @ Mode(1) >>
+            Mode(1) @ BS_hadamard @ Mode(1) >>
+            Mode(2) @ Swap(Mode(1), Mode(1)) >>
+            Mode(1) @ Swap(Mode(1), Mode(1)) @ Mode(1)
+        )
 
-classical_function_I = ClassicalFunction(
-    fusion_I_function,
-    Mode(2),
-    Bit(2)
-)
+        fusion_I = Channel(
+            "Fusion I", kraus_map_fusion_I
+        )
 
-fusion_type_I = (
-    fusion_I >>
-    qmode**2 @ NumberResolvingMeasurement(2) >>
-    qmode**2 @ classical_function_I
-)
+        def fusion_I_function(x):
+            """
+            A classical function that returns two bits based on an input x,
+            based on the classical logical for the Fusion type I circuit.
+            """
+            a = x[0]
+            b = x[1]
+            s = (a % 2) ^ (b % 2)
+            k = int(s*b + (1-s)*(1 - (a + b)/2))%2
+            return [s, k]
 
-fusion_II = Channel(
-    "Fusion",
-    (
-        BS_hadamard @ BS_hadamard >>
-        kraus_map_fusion_I >>
-        BS_hadamard @ Mode(2)
-    )
-)
+        classical_function_I = ClassicalFunction(
+            fusion_I_function,
+            Mode(2),
+            Bit(2)
+        )
 
-def fusion_II_function(x):
-    """
-    A classical function that returns two bits based on an input x,
-    based on the classical logical for the Fusion type II circuit.
-    """
-    a = x[0]
-    b = x[1]
-    c = x[2]
-    d = x[3]
-    s = (a % 2) ^ (b % 2)
-    k = int(s*(b + d) + (1-s)*(1 - (a + b)/2))%2
-    return [s, k]
+        return (
+            fusion_I >>
+            qmode**2 @ NumberResolvingMeasurement(2) >>
+            qmode**2 @ classical_function_I
+        )
 
-classical_function_II = ClassicalFunction(
-    fusion_II_function,
-    Mode(4),
-    Bit(2)
-)
 
-fusion_type_II = (
-    fusion_II >>
-    NumberResolvingMeasurement(4) >>
-    classical_function_II
-)
+class FusionTypeII(Circuit):
+    def __new__(cls):
+        fusion_II = Channel(
+            "Fusion II",
+            (
+                BS_hadamard @ BS_hadamard >>
+                Mode(1) @ Swap(Mode(1), Mode(1)) @ Mode(1) >>
+                Mode(1) @ BS_hadamard @ Mode(1) >>
+                Mode(2) @ Swap(Mode(1), Mode(1)) >>
+                Mode(1) @ Swap(Mode(1), Mode(1)) @ Mode(1) >>
+                BS_hadamard @ Mode(2)
+            )
+        )
+
+        def fusion_II_function(x):
+            """
+            A classical function that returns two bits based on an input x,
+            based on the classical logical for the Fusion type II circuit.
+            """
+            a = x[0]
+            b = x[1]
+            d = x[3]
+            s = (a % 2) ^ (b % 2)
+            k = int(s*(b + d) + (1-s)*(1 - (a + b)/2))%2
+            return [s, k]
+
+        classical_function_II = ClassicalFunction(
+            fusion_II_function,
+            Mode(4),
+            Bit(2)
+        )
+
+        return (
+            fusion_II >>
+            NumberResolvingMeasurement(4) >>
+            classical_function_II
+        )
