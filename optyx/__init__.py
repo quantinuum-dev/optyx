210b674ca75c4b55531163c4fc79f8fe125215ae
mateuszkupper <mateusz.kupper@gmail.com>
2025-06-26 13:44:42 +0100

    Init edit

--- optyx/__init__.py
+++ optyx/__init__.py
@@ -1,9 +1,8 @@
-# from optyx.diagram import zw
-# from optyx.diagram import zx
-# from optyx.diagram import lo
-# from optyx.diagram import path
+from optyx.core import zw
+from optyx.core import zx
+from optyx.core import path
 
-# from optyx._version import (
-#     version as __version__,
-#     version_tuple as __version_info__,
-# )
+from optyx._version import (
+    version as __version__,
+    version_tuple as __version_info__,
+)
--- optyx/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from optyx.core import zw
-from optyx.core import zx
-from optyx.core import path
-
-from optyx._version import (
-    version as __version__,
-    version_tuple as __version_info__,
-)
