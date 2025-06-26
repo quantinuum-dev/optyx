9c80a6b38c7cfdb53fc498e61eac40e6688b045d
mateuszkupper <mateusz.kupper@gmail.com>
2025-06-26 13:39:46 +0100

    init modified

---
+++ optyx/core/__init__.py
@@ -0,0 +1,8 @@
+from optyx.core import zw
+from optyx.core import zx
+from optyx.core import path
+
+from optyx._version import (
+    version as __version__,
+    version_tuple as __version_info__,
+)
