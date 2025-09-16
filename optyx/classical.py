69a64bfc7c03571c4fa1dd6ffcf4543891afc4af
mateuszkupper <mateusz.kupper@gmail.com>
2025-09-16 19:54:17 +0100

    an example notebook

--- optyx/classical.py
+++ optyx/classical.py
@@ -192,7 +192,7 @@ class BitControlledGate(Channel):
             )
 
             super().__init__(
-                f"BitControlledGate({control_gate}, {default_gate})",
+                f"BitControlledGate",
                 kraus,
                 bit @ tp**len(control_gate_single.dom),
                 tp**len(control_gate_single.cod)
